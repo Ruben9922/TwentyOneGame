22ecaedfbbf039ca12b1f2d13b43d4aea069b3c3
Ruben9922 <Ruben9922@users.noreply.github.com>
2017-01-15 20:55:13 +0000

    Create Suit, Rank and Card classes

---
+++ src/TwentyOneGame.py
@@ -0,0 +1,38 @@
+from enum import Enum
+
+
+class Suit(Enum):
+    Clubs, Diamonds, Hearts, Spades = range(4)
+
+    def __str__(self):
+        return self._name_
+
+
+class Rank(Enum):
+    Ace = 1,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine = 9
+    Ten = 10,
+    Jack = 11,
+    Queen = 12,
+    King = 13
+
+    def __str__(self):
+        return self._name_;
+
+
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return "{} of {}".format(str(self.rank), str(self.suit))
+
+print(str(Card(Suit.Clubs, Rank.Ace)))
